8aba80116ee4ab0c5d6f8f6c64f3dc7fefa28754
GayaniGunasekara <gayanigunasekara2001@gmail.com>
2025-10-06 22:32:43 +0530

    LAB1- Variables

---
+++ LAB01.js
@@ -0,0 +1,13 @@
+let name1 = "Maxwell Wright";
+let phone1 = "0191 7196495";
+let email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+let name2 = "Raja Villarreal";
+let phone2 = "0866 3982895";
+let email2 = "posuere.vulputate@sed.com";
+
+let name3 = "Helen RIchards";
+let phone3 = "08001111";
+let email3 = "libero@convallis.edu";
+
+console.log("name : " + name1 + " phone :" + phone1 + "email" + email1);
